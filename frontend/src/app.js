0b698e6a7e8268f62fab4a4c2c4e7cb5d367045b
Harshitha-Dantla <153969846+Harshitha-Dantla@users.noreply.github.com>
2025-06-24 08:37:52 +0530

    Added practice folder

---
+++ frontend/src/app.js
@@ -0,0 +1,9 @@
+const quotes = [
+  "Believe in yourself!",
+  "Stay positive.",
+  "Work hard in silence.",
+  "Success is not final, failure is not fatal.",
+  "Keep going no matter what!",
+];
+alert(quotes[Math.floor(Math.random() * quotes.length)]);
+console.log(" hello");
